6eab00de8018ca6372e015eb572a1328d2d77f8c
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2021-07-25 16:55:18 -0300

    Add artificial expansion of training data

--- src/convolutional.py
+++ src/convolutional.py
@@ -1,4 +1,5 @@
 # %%
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
@@ -8,6 +9,30 @@ from tensorflow import keras
 
 x_train = x_train.reshape((60000, 28, 28, 1)) / 255
 x_test = x_test.reshape((10000, 28, 28, 1)) / 255
+
+n_train = x_train.shape[0]
+x_train_expanded = np.zeros((5*n_train, 28, 28, 1), np.float64)
+y_train_expanded = np.zeros((5*n_train,), np.uint8)
+for i in range(n_train):
+    x_train_expanded[5*i] = x_train[i]
+    x_train_expanded[5*i+1, :-1, :] = x_train[i, 1:, :]
+    x_train_expanded[5*i+2, 1:, :] = x_train[i, :-1, :]
+    x_train_expanded[5*i+3, :, :-1] = x_train[i, :, 1:]
+    x_train_expanded[5*i+4, :, 1:] = x_train[i, :, :-1]
+
+    y_train_expanded[5*i:5*i+5] = y_train[i]
+
+n_test = x_test.shape[0]
+x_test_expanded = np.zeros((5*n_test, 28, 28, 1), np.float64)
+y_test_expanded = np.zeros((5*n_test,), np.uint8)
+for i in range(n_test):
+    x_test_expanded[5*i] = x_test[i]
+    x_test_expanded[5*i+1, :-1, :] = x_test[i, 1:, :]
+    x_test_expanded[5*i+2, 1:, :] = x_test[i, :-1, :]
+    x_test_expanded[5*i+3, :, :-1] = x_test[i, :, 1:]
+    x_test_expanded[5*i+4, :, 1:] = x_test[i, :, :-1]
+
+    y_test_expanded[5*i:5*i+5] = y_test[i]
 
 
 # %%
@@ -62,8 +87,8 @@ model.compile(
 )
 
 model.fit(
-    x=x_train,
-    y=y_train,
+    x=x_train_expanded,
+    y=y_train_expanded,
     epochs=60,
     batch_size=10,
     validation_split=0.2,
